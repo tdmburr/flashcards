254deda8b16cdba39555b2b299b91b9c561e89f4
Trey Marshall <marshall.trey92@yahoo.com>
2023-03-23 12:28:02 -0500

    feat: Added Game class tests and implementation code, also linked the game to be run in index.js

--- src/Game.js
+++ src/Game.js
@@ -1,18 +1,38 @@
 const data = require('./data');
 const prototypeQuestions = data.prototypeData;
 const util = require('./util');
+const Deck = require('../src/Deck')
+const Card = require('../src/Card')
+const Round = require('../src/Round')
+
+let deck;
 
 class Game {
-  constructor() {}
-
+  constructor() {
+    this.currentRound; 
+  }
   printMessage(deck, round) {
     console.log(`Welcome to FlashCards! You are playing with ${deck.countCards()} cards.
 -----------------------------------------------------------------------`)
   }
-
   printQuestion(round) {
       util.main(round);
   }
+  useForStart() {
+    let cards = []
+    prototypeQuestions.forEach(card => {
+    cards.push(new Card(card.id, card.question, card.answers, card.correctAnswer))
+    })
+    deck = new Deck(cards)
+    this.currentRound = new Round(deck)
+  }
+  start() {
+    this.useForStart()
+    this.printMessage(deck, this.currentRound)
+    this.printQuestion(this.currentRound)
+  }
 }
+
+
 
 module.exports = Game;
